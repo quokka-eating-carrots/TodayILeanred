e2b9706a7a335e6aa424e2cb87be454d913a9d11
quokka-eating-carrots <ovzip7@gmail.com>
2022-11-19 22:41:52 +0900

    Study programers

--- main.js
+++ main.js
@@ -1,84 +1,87 @@
-// 배열 회전시키기
-// function solution(numbers, direction) {
-//   let answer = [];
-//   if (direction === 'right') {
-//     numbers.unshift(numbers.pop())
-//   } else {
-//     numbers.push(numbers.shift())
+// 중복된 문자 제거
+// function solution(my_string) {
+//   let answer = '';
+//   const set = new Set(my_string)
+//   for (let x of set) {
+//     answer += x
 //   }
-//   answer = numbers
 //   return answer;
 // }
 
-// console.log(solution([1, 2, 3], 'right'))
-// console.log(solution([4, 455, 6, 4, -1, 45, 6], 'left'))
+// console.log(solution('people'))
+// console.log(solution('We are the world'))
 
-// 최댓값 만들기 (2)
-// function solution(numbers) {
-//   let answer = 0;
-//   let max = Number.MIN_SAFE_INTEGER;
-//   for (let i = 0; i < numbers.length - 1; i += 1) {
-//     for (let j = 1; j < numbers.length; j += 1) {
-//       if (max < numbers[i] * numbers[j]) {
-//         if (i !== j) {
-//           max = numbers[i] * numbers[j]
-//         }
+// 모스 부호(1)
+// function solution(letter) {
+//   const morse = { 
+//     '.-':'a',
+//     '-...':'b',
+//     '-.-.':'c',
+//     '-..':'d',
+//     '.':'e',
+//     '..-.':'f',
+//     '--.':'g',
+//     '....':'h',
+//     '..':'i',
+//     '.---':'j',
+//     '-.-':'k',
+//     '.-..':'l',
+//     '--':'m',
+//     '-.':'n',
+//     '---':'o',
+//     '.--.':'p',
+//     '--.-':'q',
+//     '.-.':'r',
+//     '...':'s',
+//     '-':'t',
+//     '..-':'u',
+//     '...-':'v',
+//     '.--':'w',
+//     '-..-':'x',
+//     '-.--':'y',
+//     '--..':'z'
+//   }
+//   let answer = '';
+//   letter = letter.split(' ')
+//   for (let x of letter) {
+//     for (let key in morse) {
+//       if (x === key) {
+//         answer += morse[key]
 //       }
-//       answer = max
 //     }
 //   }
 //   return answer;
 // }
 
-// console.log(solution([1, 2, -3, 4, -5]))
-// console.log(solution([0, -31, 24, 10, 1, 9]))
-// console.log(solution([10, 20, 30, 5, 5, 20, 5]))
+// console.log(solution('.... . .-.. .-.. ---'))
+// console.log(solution(".--. -.-- - .... --- -."))
 
-// 369게임
-// function solution(order) {
-//   let answer = 0;
-//   order = order.toString()
-//   for (let x of order) {
-//     if (x !== '0' && x % 3 === 0) {
-//       answer += 1
-//     }
-//   }
-//   return answer;
-// }
-
-// console.log(solution(3))
-// console.log(solution(29423))
-// console.log(solution(1000))
-
-// 합성수 찾기
+// 팩토리얼
 // function solution(n) {
-//   let answer = 0
-//   const isPrime = num => {
-//     for (let i = 2; i <= Math.sqrt(num); i++) {
-//       if (num % i === 0) return true;
+//   let answer = 0;
+//   let cnt = 1
+//   for (let i = 1; i <= 10; i += 1) {
+//     cnt = cnt * i
+//     if (cnt <= n) {
+//       answer = i
 //     }
-//     return false;
-//   };
-
-//   for (let i = 1; i <= n; i++) {
-//     if (isPrime(i)) answer += 1;
 //   }
-
 //   return answer;
 // }
 
-function solution(n) {
-  let answer = 0
-  for (let i = 1; i <= n; i += 1) {
-    for (let j = 2; j <= Math.sqrt(i); j += 1) {
-      if (i % j === 0) {
-        answer += 1
-        break;
-      }
-    }
-  }
-  return answer
+// console.log(solution(3628800))
+// console.log(solution(7))
+
+// A로 B 만들기
+function solution(before, after) {
+  let answer = 0;
+  before = before.split('').sort().join('')
+  after = after.split('').sort().join('')
+  if (before === after) {
+    answer = 1
+  } else answer = 0
+  return answer;
 }
 
-console.log(solution(10))
-console.log(solution(15))
+console.log(solution('olleh', 'hello'))
+console.log(solution('allpe', 'apple'))
