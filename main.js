3fb4bddbe944faedbd5a4d7e9313a01f23f89977
quokka-eating-carrots <ovzip7@gmail.com>
2022-11-29 13:02:07 +0900

    Study Programers

--- main.js
+++ main.js
@@ -1,13 +1,52 @@
-const str = `
-010-1234-5678
-thesecon@gmail.com
-Hello world!
-https://www.omdbapi.com/?apikey=7035c60c&s=frozen
-The quick brown fox jumps over the lazy dog.
-hello@naver.com
-http://localhost:1234
-동해물과 백두산이 마르고 닳도록
-abbcccddddeeeee
-`
+// 약수의 합
+// function solution(n) {
+//   let answer = 0;
+//   for (let i = 1; i <= n; i += 1) {
+//     if (n % i === 0) {
+//       answer += i
+//     }
+//   }
+//   return answer;
+// }
 
-console.log(/the/gi.test(str))
+// console.log(solution(12))
+// console.log(solution(5))
+
+// 평균 구하기
+// function solution(arr) {
+//   let answer = 0;
+//   let sum = 0
+//   for (let x of arr) {
+//     sum += x
+//   }
+//   answer = sum / arr.length
+//   return answer;
+// }
+
+// console.log(solution([1, 2, 3, 4]))
+// console.log(solution([5, 5]))
+
+// 짝수와 홀수
+// function solution(num) {
+//   let answer = '';
+//   if (num % 2 === 0) {
+//     answer = "Even"
+//   } else answer = "Odd"
+//   return answer;
+// }
+
+// console.log(solution(3))
+// console.log(solution(4))
+
+// 자릿수 더하기
+function solution(n) {
+  let answer = 0;
+  n = String(n)
+  for (let x of n) {
+    answer += Number(x)
+  }
+  return answer;
+}
+
+console.log(solution(123))
+console.log(solution(987))
