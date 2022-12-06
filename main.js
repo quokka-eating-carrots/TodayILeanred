058af6971fc88735234f146a42ff38009dca21b4
quokka-eating-carrots <ovzip7@gmail.com>
2022-12-06 13:27:34 +0900

    Study Programers

--- main.js
+++ main.js
@@ -1,96 +1,94 @@
-// 약수의 개수와 덧셈
-// function solution(left, right) {
-//   let answer = 0;
-//   let num = []
-//   for (let i = left; i <= right; i += 1) {
-//     num.push(i)
+// 직사각형 별 찍기
+// process.stdin.setEncoding("utf8");
+// process.stdin.on("data", (data) => {
+//   const n = data.split(" ");
+//   const a = Number(n[0]),
+//     b = Number(n[1]);
+//   console.log(("*".repeat(a) + `\n`).repeat(b));
+// });
+
+// 최대공약수와 최소공배수
+// function solution(n, m) {
+//   var answer = [];
+//   let bigNum = 0
+//   let smallNum = 0
+//   if (n > m) {
+//     bigNum = n
+//     smallNum = m
+//   } else {
+//     bigNum = m
+//     smallNum = n
 //   }
-//   let cnt = 0
-//   let divisor = []
-//   for (let x of num) {
-//     for (let i = 1; i <= x; i += 1) {
-//       if (x % i === 0) {
-//         cnt += 1
+//   let gcd = 0
+//   for (let i = 1; i <= bigNum; i += 1) {
+//     if (bigNum % i === 0 && smallNum % i === 0) {
+//       if (gcd < i) {
+//         gcd = i
 //       }
 //     }
-//     divisor.push(cnt)
-//     cnt = 0
 //   }
-//   for (let i = 0; i < divisor.length; i += 1) {
-//     if (divisor[i] % 2 === 0) {
-//       answer += num[i]
-//     } else answer -= num[i]
+//   let lcm = 0
+//   let bigNumArr = []
+//   let smallNumArr = []
+//   for (let i = 1; i <= bigNum; i += 1) {
+//     bigNumArr.push(bigNum * i)
+//     smallNumArr.push(smallNum * i)
 //   }
-//   return answer;
-// }
-
-// console.log(solution(13, 17))
-// console.log(solution(24, 27))
-
-// 문자열 다루기 기본
-// function solution(s) {
-//   var answer = true;
-//   if (s.length === 4) {
-//     for (let x of s) {
-//       if (x.charCodeAt() > 57) {
-//         return answer = false
+//   for (let i = 0; i < bigNumArr.length; i += 1) {
+//     for (let j = 0; j < smallNumArr.length; j += 1) {
+//       if (bigNumArr[i] === smallNumArr[j]) {
+//         lcm = bigNumArr[i]
+//         break;
 //       }
 //     }
-//   } else if (s.length === 6) {
-//     for (let x of s) {
-//       if (x.charCodeAt() > 57) {
-//         return answer = false
-//       }
-//     }
-//   } else return answer = false
-//   return answer;
-// }
-
-// function solution(s) {
-//   var answer = true;
-//   if (s.length === 4 || s.length === 6) {
-//     for (let x of s) {
-//       if (x.charCodeAt() > 57) {
-//         return answer = false
-//       }
-//     }
-//   } else return answer = false
-//   return answer;
-// }
-
-// console.log(solution('a234'))
-// console.log(solution('1234'))
-
-// 부족한 금액 계산하기
-// function solution(price, money, count) {
-//   var answer = 0;
-//   let sumPrice = 0
-//   for (let i = 1; i <= count; i += 1) {
-//     sumPrice += price * i
 //   }
-//   answer = sumPrice - money
-//   if (sumPrice < money) {
-//     answer = 0
-//   }
-//   return answer;
+//   console.log(bigNumArr, smallNumArr)
+//   return answer = [gcd, lcm];
 // }
 
-// console.log(solution(3, 20, 4))
+// function solution(n, m) {
+//   let gcd = (n, m) => (n % m === 0 ? m : gcd(m, n % m))
+//   let lcm = (n, m) => (n * m) / gcd(n, m)
+//   return [gcd(n, m), lcm(n, m)]
+// }
 
-// 행렬의 덧셈
-function solution(arr1, arr2) {
-  let answer = [];
-  let sum = []
-  for (let i = 0; i < arr1.length; i += 1) {
-    for (let j = 0; j < arr1[i].length; j += 1) {
-      sum.push(arr1[i][j] + arr2[i][j])
+// console.log(solution(3, 12))
+// console.log(solution(2, 5))
+
+// 같은 숫자는 싫어
+// function solution(arr) {
+//   let stack = []
+//   for (let x of arr) {
+//     if (stack.length > 0 && stack[stack.length - 1] === x) {
+//       stack.pop()
+//       stack.push(x)
+//     } else stack.push(x)
+//   }
+//   return stack;
+// }
+
+// console.log(solution([1, 1, 3, 3, 0, 1, 1]))
+// console.log(solution([4, 4, 4, 3, 3]))
+
+// 이상한 문자 만들기
+function solution(s) {
+  var answer = '';
+  let cnt = 0
+  for (let i = 0; i < s.length; i += 1) {
+    if (s[i] === ' ') {
+      answer += s[i]
+      cnt = 0
+    } else if (cnt % 2 === 0) {
+      cnt += 1
+      answer += s[i].toUpperCase()
+    } else if (cnt % 2 !== 0) {
+      cnt += 1
+      answer += s[i].toLowerCase()
     }
-    answer.push(sum)
-    sum = []
   }
-  console.log(sum)
   return answer;
 }
 
-console.log(solution([[1, 2], [2, 3]], [[3, 4], [5, 6]]))
-console.log(solution([[1], [2]], [[3], [4]]))
+console.log(solution("try hello world"))
+console.log(solution("abcd efg hijk"))
+console.log(solution("  HEf efer sErgd  "))
