2ba208097185ea228c845646d23973bd8b607985
quokka-eating-carrots <ovzip7@gmail.com>
2022-12-29 14:03:06 +0900

    Study Programers

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TodayILeanred
+221229 [Lv. 1 명예의 전당](https://school.programmers.co.kr/learn/courses/30/lessons/138477)
+
+[풀이 참고 사이트](https://velog.io/@kwb020312/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%AA%85%EC%98%88%EC%9D%98-%EC%A0%84%EB%8B%B9-1)
--- index.js
+++ index.js
@@ -1,69 +1,69 @@
-// 숫자 짝꿍
-// function solution(X, Y) {
-//   let answer = '';
-//   for (let x of X) {
-//     for (let i = 0; i < Y.length; i += 1) {
-//       if (x === Y[i]) {
-//         Y = Y.split('')
-//         Y.splice(i, 1)
-//         Y = Y.join('')
-//         answer += x
-//         break;
-//       }
+// 명예의 전당
+// function solution(k, score) {
+//   let answer = [];
+//   answer.push(score[0])
+//   for (let i = 1; i < k; i += 1) {
+//     let min = Math.min(...answer)
+//     if (min > score[i] && score[i] >= 10) {
+//       answer.push(score[i])
+//     } else answer.push(min)
+//   }
+
+//   for (let i = k; i < score.length; i += 1) {
+//     let min = []
+//     for (let j = 0; j > -k; j -= 1) {
+//       min.push(score[i + j])
 //     }
+//     min.sort((a, b) => a - b)
+//     if (min[0] < 10) {
+//       answer.push(min[1])
+//     } else answer.push(min[0])
 //   }
-//   if (answer.length === 0) {
-//     return answer = "-1"
-//   } else {
-//     answer = answer.split('').sort((a, b) => b - a).join('')
-//     answer = +answer
-//     return answer = answer.toString()
-//   }
+//   return answer;
 // }
 
-// function solution(X, Y) {
-//   const commons = []
-//   const obj = {}
+// console.log(solution(3, [10, 100, 20, 150, 1, 100, 200]))
+console.log(solution(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]))
 
-//   for (const el of X) {
-//     obj[el] = (obj[el] || 0) + 1
+// function solution(k, score) {
+//   let answer = [];
+//   answer.push(score[0])
+//   for (let i = 1; i < k; i += 1) {
+//     let min = Math.min(...answer)
+//     if (min > score[i] && score[i] >= 10) {
+//       answer.push(score[i])
+//     } else answer.push(min)
 //   }
 
-//   for (const el of Y) {
-//     if (obj[el]) {
-//       commons.push(el)
-//       obj[el] -= 1
+//   for (let i = k; i < score.length; i += 1) {
+//     let min = []
+//     for (let j = 0; j > -k; j -= 1) {
+//       min.push(score[i + j])
 //     }
+//     min.sort((a, b) => a - b)
+//     console.log(min)
+//     if (min[0] < 10 || answer.includes(min[0])) {
+//       answer.push(min[1])
+//     } else if (min[0] < answer[answer.length - 1]) {
+//       answer.push(answer[answer.length - 1])
+//     } else answer.push(min[0])
 //   }
-
-//   commons.sort((a, b) => b - a)
-//   if (!commons.length) return "-1"
-//   else if (commons[0] === "0") return "0"
-//   return commons.join("")
+//   return answer;
 // }
 
-function solution(X, Y) {
-  let answer = []
-  let mapX = new Map()
-  for (let x of X) {
-    mapX.set(x, (mapX.get(x) || 0) + 1)
-  }
-
-  for (let y of Y) {
-    if (mapX.has(y) && mapX.get(y) !== 0) {
-      answer.push(y)
-      mapX.set(y, mapX.get(y) - 1)
+function solution(k, score) {
+  const answer = []
+  const honor = []
+  for (let i = 0; i < score.length; i += 1) {
+    if (i < k) {
+      honor.push(score[i])
     }
+    if (score[i] > Math.min(...honor)) {
+      honor.pop()
+      honor.push(score[i])
+      honor.sort((a, b) => b - a)
+    }
+    answer.push(honor.at(-1))
   }
-
-  answer.sort((a, b) => b - a)
-  if (!answer.length) return "-1"
-  else if (answer[0] === "0") return "0"
-  return answer.join("")
+  return answer
 }
-
-console.log(solution("100", "2345"))
-console.log(solution("100", "203045"))
-console.log(solution("100", "123450"))
-console.log(solution("12321", "42531"))
-console.log(solution("5525", "1255"))
