5ba03677eb6451b99f2ddb9126acf633908868cb
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-01-03 13:16:01 +0900

    [unrated] Title: 옹알이 (2), Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,15 @@
+function solution(babbling) {
+  const r1 = /(aya|ye|woo|ma)\1/
+  const r2 = /^(aya|ye|woo|ma)+$/
+  let answer = 0
+  for (let x of babbling) {
+    const m = r1.exec(x)
+    if (m) continue
+
+    const m2 = r2.exec(x)
+    if (!m2) continue
+
+    answer += 1
+  }
+  return answer
+}
