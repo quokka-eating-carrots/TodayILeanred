734df2b9b240f524b89a329ab54206393305ce41
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-04 13:13:19 +0900

    Study Programers

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # TodayILeanred
-230103 [Lv. 1 옹알이(2)](https://school.programmers.co.kr/learn/courses/30/lessons/133499)
+230103 [Lv. 1 문자열 나누기](https://school.programmers.co.kr/learn/courses/30/lessons/140108)
 
-[풀이 참고 사이트](https://school.programmers.co.kr/questions/40298)
+[풀이 참고 사이트](https://leejams.github.io/%EB%AC%B8%EC%9E%90%EC%97%B4-%EB%82%98%EB%88%84%EA%B8%B0/)
--- index.js
+++ index.js
@@ -1,35 +1,50 @@
-// 옹알이 (2)
-// function solution(babbling) {
-//   var answer = 0;
-//   for (let x of babbling) {
-//     if (x.replace("ye", " ").replace("aya", " ").replace("woo", " ").replace("ma", " ").trim() === "") {
+// 문자열 나누기
+// function solution(s) {
+//   let answer = 0
+//   let sameCnt = 0
+//   let diffCnt = 0
+//   for (let i = 0; i < s.length; i += 1) {
+//     if (s[0] === s[i]) {
+//       console.log(i)
+//       sameCnt += 1
+//     } else {
+//       diffCnt += 1
+//     }
+//     if (sameCnt === diffCnt) {
 //       answer += 1
+//       sameCnt = 0
+//       diffCnt = 0
+//       s = s.slice(i + 1)
+//       console.log(s)
+//       i = -1
 //     }
 //   }
 //   return answer;
 // }
 
-function solution(babbling) {
-  // (그룹) + 그룹과 동일한 단어
-  // 2개의 동일한 단어를 연속적으로 사용해야만 매치된다.
-  // \1은 정규식의 그룹 중 첫 번째 그룹을 가리킨다.
-  // 연속적인 발음을 할 수 없으니까 연속적으로 발음이 나타나는 것을 걸러 주게 된다.
-  const r1 = /(aya|ye|woo|ma)\1/
-  // 그룹으로 시작하고, 하나 이상 일치하며 그룹으로 끝나야 한다.
-  const r2 = /^(aya|ye|woo|ma)+$/
+console.log(solution("banana"))
+console.log(solution("abracadabra"))
+console.log(solution("aaabbaccccabba"))
+console.log(solution("aaa"))
+
+function solution(s) {
   let answer = 0
-  for (let x of babbling) {
-    const m = r1.exec(x)
-    if (m) continue
+  let firstS = ''
+  let sameCnt = 0
+  let diffCnt = 0
+  for (let x of s) {
+    if (!firstS) firstS = x
+    if (firstS === x) sameCnt += 1
+    else diffCnt += 1
 
-    const m2 = r2.exec(x)
-    if (!m2) continue
-
-    answer += 1
+    if (sameCnt === diffCnt) {
+      answer += 1
+      sameCnt = 0
+      diffCnt = 0
+      firstS = ''
+    }
   }
-  return answer
-}
 
-// console.log(solution(["aya", "yee", "u", "maa"]))
-console.log(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]))
-// console.log(solution(["maayama", "mama"]))
+  if (firstS) answer += 1
+  return answer;
+}
