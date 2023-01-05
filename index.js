88258b5234c7974bff102c85a68074b5c953665c
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-05 14:57:13 +0900

    Study Programers 성격 유형 검사하기

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-// 문자열 나누기
-// function solution(s) {
-//   let answer = 0
-//   let sameCnt = 0
-//   let diffCnt = 0
-//   for (let i = 0; i < s.length; i += 1) {
-//     if (s[0] === s[i]) {
-//       console.log(i)
-//       sameCnt += 1
-//     } else {
-//       diffCnt += 1
-//     }
-//     if (sameCnt === diffCnt) {
-//       answer += 1
-//       sameCnt = 0
-//       diffCnt = 0
-//       s = s.slice(i + 1)
-//       console.log(s)
-//       i = -1
-//     }
-//   }
-//   return answer;
-// }
+// 성격 유형 검사하기
 
-console.log(solution("banana"))
-console.log(solution("abracadabra"))
-console.log(solution("aaabbaccccabba"))
-console.log(solution("aaa"))
-
-function solution(s) {
-  let answer = 0
-  let firstS = ''
-  let sameCnt = 0
-  let diffCnt = 0
-  for (let x of s) {
-    if (!firstS) firstS = x
-    if (firstS === x) sameCnt += 1
-    else diffCnt += 1
-
-    if (sameCnt === diffCnt) {
-      answer += 1
-      sameCnt = 0
-      diffCnt = 0
-      firstS = ''
+function solution(survey, choices) {
+  let answer = '';
+  let result = {
+    R: 0,
+    T: 0,
+    C: 0,
+    F: 0,
+    J: 0,
+    M: 0,
+    A: 0,
+    N: 0
+  }
+  for (let i = 0; i < survey.length; i += 1) {
+    if (choices[i] === 1) {
+      result[survey[i][0]] += 3
+    } else if (choices[i] === 2) {
+      result[survey[i][0]] += 2
+    } else if (choices[i] === 3) {
+      result[survey[i][0]] += 1
+    } else if (choices[i] === 5) {
+      result[survey[i][1]] += 1
+    } else if (choices[i] === 6) {
+      result[survey[i][1]] += 2
+    } else if (choices[i] === 7) {
+      result[survey[i][1]] += 3
     }
   }
 
-  if (firstS) answer += 1
+  if (result.R > result.T || result.R === result.T) {
+    answer += "R"
+  } else if (result.R < result.T) {
+    answer += "T"
+  }
+  if (result.C > result.F || result.C === result.F) {
+    answer += "C"
+  } else if (result.C < result.F) {
+    answer += "F"
+  }
+  if (result.J > result.M || result.J === result.M) {
+    answer += "J"
+  } else if (result.J < result.M) {
+    answer += "M"
+  }
+  if (result.A > result.N || result.A === result.N) {
+    answer += "A"
+  } else if (result.A < result.N) {
+    answer += "N"
+  }
   return answer;
 }
+
+console.log(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]))
+console.log(solution(["TR", "RT", "TR"], [7, 1, 3]))
