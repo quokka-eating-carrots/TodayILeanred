348d59efec554ba7bf9989f6df57e026cc926151
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-02-07 12:38:13 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 9640 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,11 @@
+var fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
+let input = fs.readFileSync(filePath).toString();
+const length = parseInt(input);
+for (let i = 1; i <= length; i += 1) {
+  let star = "";
+  for (let j = 0; j < i; j += 1) {
+    star += "*";
+  }
+  console.log(star);
+}
