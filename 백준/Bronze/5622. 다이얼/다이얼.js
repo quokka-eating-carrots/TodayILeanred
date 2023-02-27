7009804693f5b3a47cc608757e1742cea832998d
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-02-27 12:10:40 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.js
@@ -0,0 +1,24 @@
+var fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("");
+const alphabet = {
+  2: ["A", "B", "C"],
+  3: ["D", "E", "F"],
+  4: ["G", "H", "I"],
+  5: ["J", "K", "L"],
+  6: ["M", "N", "O"],
+  7: ["P", "Q", "R", "S"],
+  8: ["T", "U", "V"],
+  9: ["W", "X", "Y", "Z"],
+};
+
+let answer = 0;
+for (let x of input) {
+  for (let num in alphabet) {
+    if (alphabet[num].includes(x)) {
+      answer += +num + 1;
+    }
+  }
+}
+
+console.log(answer);
