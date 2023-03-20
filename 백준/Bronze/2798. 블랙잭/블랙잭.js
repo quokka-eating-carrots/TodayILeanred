898b852fea0b128d80c03c537d9eb5eef3bb33cb
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-03-20 12:05:55 +0900

    [Bronze II] Title: 블랙잭, Time: 188 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -6,10 +6,12 @@ const limit = Number(input[0].split(" ")[1]);
 const nums = input[1].split(" ");
 let max = 0;
 for (let i = 0; i < numsLeng; i += 1) {
-  for (let j = i + 1; j < numsLeng; j += 1) {
-    for (let k = j + 1; k < numsLeng; k += 1) {
-      let sum = +nums[i] + +nums[j] + +nums[k];
-      if (sum > max && sum <= limit) max = sum;
+  for (let j = 0; j < numsLeng; j += 1) {
+    for (let k = 0; k < numsLeng; k += 1) {
+      if (i !== j && j !== k && i !== k) {
+        const plusNum = +nums[i] + +nums[j] + +nums[k];
+        if (plusNum > max && plusNum <= limit) max = plusNum;
+      }
     }
   }
 }
