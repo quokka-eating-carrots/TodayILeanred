2da7032e1c59293dc870959ff30706eb2e1ad8a2
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-05-08 19:16:56 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,11 @@
+function solution(a, b, c) {
+    var answer = 0;
+    if (a === b && b === c && a === c) {
+        answer = (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c)
+    } else if (a !== b && b !== c && a !== c) {
+        answer = a + b + c
+    } else {
+        answer = (a + b + c) * (a * a + b * b + c * c)
+    }
+    return answer;
+}
