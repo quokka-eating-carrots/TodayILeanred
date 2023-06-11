cc8a5ce9b614901e8e69ff27338acf6ca3d8d638
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-06-11 19:38:31 +0900

    [level 2] Title: 피로도, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -0,0 +1,17 @@
+function solution(k, dungeons) {
+  var answer = 0;
+  const visited = Array.from({ length: dungeons.length }, () => false);
+
+  function DFS(hp, L) {
+    for (let i = 0; i < dungeons.length; i += 1) {
+      if (!visited[i] && dungeons[i][0] <= hp) {
+        visited[i] = true;
+        DFS(hp - dungeons[i][1], L + 1);
+        visited[i] = false;
+      }
+    }
+    answer = Math.max(answer, L);
+  }
+  DFS(k, 0);
+  return answer;
+}
