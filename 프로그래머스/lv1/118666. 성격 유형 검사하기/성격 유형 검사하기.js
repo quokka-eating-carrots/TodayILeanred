4f916e6cdb6de2e97b28dca3508f85fb9eb70ff0
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-07-31 17:02:33 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.53 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,50 +1,62 @@
-function solution(survey, choices) {
-  let answer = '';
-  let result = {
-    R: 0,
-    T: 0,
-    C: 0,
-    F: 0,
-    J: 0,
-    M: 0,
-    A: 0,
-    N: 0
-  }
+function solution(survey, choice) {
+  let answer = "";
+  const result = [
+    { R: 0, T: 0 },
+    { C: 0, F: 0 },
+    { J: 0, M: 0 },
+    { A: 0, N: 0 },
+  ];
+
   for (let i = 0; i < survey.length; i += 1) {
-    if (choices[i] === 1) {
-      result[survey[i][0]] += 3
-    } else if (choices[i] === 2) {
-      result[survey[i][0]] += 2
-    } else if (choices[i] === 3) {
-      result[survey[i][0]] += 1
-    } else if (choices[i] === 5) {
-      result[survey[i][1]] += 1
-    } else if (choices[i] === 6) {
-      result[survey[i][1]] += 2
-    } else if (choices[i] === 7) {
-      result[survey[i][1]] += 3
+    if (survey[i].includes("R")) {
+      if (choice[i] < 4) {
+        result[0][survey[i][0]] += 4 - choice[i];
+      } else if (choice[i] > 3) {
+        result[0][survey[i][1]] += choice[i] - 4;
+      }
+    } else if (survey[i].includes("C")) {
+      if (choice[i] < 4) {
+        result[1][survey[i][0]] += 4 - choice[i];
+      } else if (choice[i] > 3) {
+        result[1][survey[i][1]] += choice[i] - 4;
+      }
+    } else if (survey[i].includes("J")) {
+      if (choice[i] < 4) {
+        result[2][survey[i][0]] += 4 - choice[i];
+      } else if (choice[i] > 3) {
+        result[2][survey[i][1]] += choice[i] - 4;
+      }
+    } else if (survey[i].includes("A")) {
+      if (choice[i] < 4) {
+        result[3][survey[i][0]] += 4 - choice[i];
+      } else if (choice[i] > 3) {
+        result[3][survey[i][1]] += choice[i] - 4;
+      }
     }
   }
 
-  if (result.R > result.T || result.R === result.T) {
-    answer += "R"
-  } else if (result.R < result.T) {
-    answer += "T"
+  if (result[0].R > result[0].T || result[0].R === result[0].T) {
+    answer += "R";
+  } else {
+    answer += "T";
   }
-  if (result.C > result.F || result.C === result.F) {
-    answer += "C"
-  } else if (result.C < result.F) {
-    answer += "F"
+
+  if (result[1].C > result[1].F || result[1].C === result[1].F) {
+    answer += "C";
+  } else {
+    answer += "F";
   }
-  if (result.J > result.M || result.J === result.M) {
-    answer += "J"
-  } else if (result.J < result.M) {
-    answer += "M"
+
+  if (result[2].J > result[2].M || result[2].J === result[2].M) {
+    answer += "J";
+  } else {
+    answer += "M";
   }
-  if (result.A > result.N || result.A === result.N) {
-    answer += "A"
-  } else if (result.A < result.N) {
-    answer += "N"
+
+  if (result[3].A > result[3].N || result[3].A === result[3].N) {
+    answer += "A";
+  } else {
+    answer += "N";
   }
   return answer;
 }
