e15e90164f1601776a5068f9c9b5450c4e3e107a
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-08-14 15:12:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,23 +1,15 @@
 function solution(s) {
-  var answer = '';
-  const newS = s.split(" ")
-  for (let x of newS) {
+  let answer = "";
+  const str = s.split(" ");
+  for (let x of str) {
     for (let i = 0; i < x.length; i += 1) {
       if (i === 0) {
-        if (48 <= x[0].charCodeAt() && x[0].charCodeAt() <= 57) {
-          answer += x[0]
-        } else {
-          answer += x[0].toUpperCase()
-        }
+        answer += x[i].toUpperCase();
       } else {
-        answer += x[i].toLowerCase()
+        answer += x[i].toLowerCase();
       }
-      // if (i === x.length - 1 && newS.indexOf(x) !== newS.length - 1) {
-      //   answer += " "
-      // }
     }
-    answer += " "
+    answer += " ";
   }
-  answer = answer.slice(0, answer.length - 1)
-  return answer;
+  return answer.slice(0, answer.length - 1);
 }
