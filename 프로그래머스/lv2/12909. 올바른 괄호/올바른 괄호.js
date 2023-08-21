d43bfb4596d699d17cab715bfddd90d36cd646cb
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-08-21 16:14:15 +0900

    [level 2] Title: 올바른 괄호, Time: 4.93 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,9 +1,9 @@
 function solution(s) {
-  if (s.length === 1 || s[0] === ")") return false
-  let cnt = 0
+  if (s.length === 1 || s[0] === ")") return false;
+  let cnt = 0;
   for (let x of s) {
-    x === "(" ? cnt += 1 : cnt -= 1
+    x === "(" ? (cnt += 1) : (cnt -= 1);
     if (cnt < 0) break;
   }
-  return cnt === 0 ? true : false
+  return cnt === 0 ? true : false;
 }
