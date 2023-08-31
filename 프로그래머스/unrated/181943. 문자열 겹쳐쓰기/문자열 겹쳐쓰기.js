e3f43d4814c319799490c112980f2f594d192f32
MinJeong Jo <113992260+quokka-eating-carrots@users.noreply.github.com>
2023-08-31 11:43:54 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,7 @@
 function solution(my_string, overwrite_string, s) {
-	let answer = my_string.split("")
-	answer.splice(s, overwrite_string.length, overwrite_string)
-	return answer.join("")
+  return (
+    my_string.slice(0, s) +
+    overwrite_string +
+    my_string.slice(s + overwrite_string.length)
+  );
 }
